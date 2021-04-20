7176a7c958396e477c25ba1498289ebd34fc3e4f
Roque Ribeiro da Silva <roque.ribeiro@hotmail.com.br>
2021-04-20 14:28:22 -0300

    Add more comments

--- mparticle.php
+++ mparticle.php
@@ -17,13 +17,14 @@ require_once(plugin_dir_path(__FILE__) . 'class.mparticle.php');
 
 function insert_scripts()
 {
+    // Create fake script element with authentication data parameters
     wp_enqueue_script('auth_parameters', plugin_dir_url(__FILE__) . '#', array(), '1.0.0', false);
 
     wp_enqueue_script('load_sample', plugin_dir_url(__FILE__) . 'events/load_sample.js', array(), '1.0.0', false);
     wp_enqueue_script('click_sample', plugin_dir_url(__FILE__) . 'events/click_sample.js', array(), '1.0.0', false);
 }
 
-
+// Add encripted data to fake element of authentication
 function add_data_attribute($tag, $handle)
 {
     $auth_host = MParticle::auth_host();
@@ -33,6 +34,6 @@ function add_data_attribute($tag, $handle)
 }
 add_filter('script_loader_tag', 'add_data_attribute', 10, 2);
 
-
+// Add scripts in wordpress
 add_action('wp_head', 'insert_scripts'); // Wordpress Pages
 add_action('admin_head', 'insert_scripts'); // Admin Panel
